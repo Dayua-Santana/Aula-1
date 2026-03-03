04f016a6c075f68638c727e88a258cf53e1aa8a5
Dayuã-Santana <daniponsantana@gmail.com>
2026-03-03 15:19:17 -0300

    Adicionando arquivos iniciais

---
+++ HelloWorld.app/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("________________________");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("________________________");
+
+Console.WriteLine("Esperando interação do Usuário......");
+
+Console.ReadLine();
+Console.WriteLine("________________________");
+Console.WriteLine("Olá, mundo!");
